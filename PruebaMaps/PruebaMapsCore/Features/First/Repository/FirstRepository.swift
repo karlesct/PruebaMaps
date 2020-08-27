fb8ee58c6b38c6ea806c5f88698354799b3ea942
ccanadas <carles.canadas.torrents@everis.com>
2020-08-27 12:01:34 +0200

    [ADD] - FirstRepository repository for the FirstFeature

---
+++ PruebaMaps/PruebaMapsCore/Features/First/Repository/FirstRepository.swift
@@ -0,0 +1,47 @@
+//
+//  FirstRepository.swift
+//  PruebaMapsCore
+//
+//  Created by Carles Cañadas Torrents on 27/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import Foundation
+
+internal protocol FirstRepositoryProtocol {
+    func getPoints(pointRequest: PointRequest, completion completed: @escaping (Result<Point, RemoteError>) -> Void)
+}
+
+internal final class FirstRepository {
+    
+    // MARK: - Properties
+    
+    private let remoteHandler: RemoteHandler
+    
+    // MARK: - Init
+    
+    init(remoteHandler: RemoteHandler) {
+        self.remoteHandler = remoteHandler
+    }
+    
+}
+
+extension FirstRepository: FirstRepositoryProtocol {
+    
+    func getPoints(pointRequest: PointRequest, completion completed: @escaping (Result<Point, RemoteError>) -> Void) {
+        
+        remoteHandler.load(Point.self,
+                           from: .getPoints(pointRequest: pointRequest)) { result in
+                            
+                            DispatchQueue.main.async {
+                                switch result {
+                                case .success(let point):
+                                    completed(.success(point))
+                                case .failure(let error):
+                                    completed(.failure(error))
+                                }
+                            }
+        }
+    }
+    
+}
