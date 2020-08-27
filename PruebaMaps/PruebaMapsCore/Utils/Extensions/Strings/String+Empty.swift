e47acd174eea2f986345c87c94df1c50a99d7db9
ccanadas <carles.canadas.torrents@everis.com>
2020-08-27 09:36:24 +0200

    [ADD] - String+Empty extension for work with empty text

---
+++ PruebaMaps/PruebaMapsCore/Utils/Extensions/Strings/String+Empty.swift
@@ -0,0 +1,15 @@
+//
+//  String+Empty.swift
+//  PruebaMapsCore
+//
+//  Created by Carles Cañadas Torrents on 27/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+
+    static let empty = ""
+
+}
