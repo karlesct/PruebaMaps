2cfe1d89c2a87e9f1e6db2229895c1d4493ff6f8
ccanadas <carles.canadas.torrents@everis.com>
2020-08-27 09:29:59 +0200

    [ADD] - Initial commit PruebaMapsCore

---
+++ PruebaMaps/PruebaMapsCore/PruebaMapsCore.h
@@ -0,0 +1,19 @@
+//
+//  PruebaMapsCore.h
+//  PruebaMapsCore
+//
+//  Created by Carles Cañadas Torrents on 27/08/2020.
+//  Copyright © 2020 carlescanadastorrents. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PruebaMapsCore.
+FOUNDATION_EXPORT double PruebaMapsCoreVersionNumber;
+
+//! Project version string for PruebaMapsCore.
+FOUNDATION_EXPORT const unsigned char PruebaMapsCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PruebaMapsCore/PublicHeader.h>
+
+
